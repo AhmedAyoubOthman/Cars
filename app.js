4a678da12beb7bbfc10f1f6068f36fa4f478939f
AhmedAyoubOthman <a18000311@gmail.com>
2021-01-28 16:38:59 +0200

    using for and while loops

--- app.js
+++ app.js
@@ -1,22 +1,56 @@
-console.log(`hello my name is ahmed and this is my firt work on js`);
+console.log('hello my name is ahmed and this is my firt work on js');
 
-var message="";
-var favoretKind = prompt(`what is your favoret kind?`);
-console.log(favoretKind);
+/* var message="";
+var favoriteKind = prompt('what is your favorite type of cars?');
+console.log(favorietKind);
 
-if (favoretKind ==  `electric`){
-    message = `You are smart`;
+ if (favoriteKind ==  'electric'){
+    message = 'You are smart';
 
-}else if (favoretKind == `Hybrid`){
-    message = `You are opin minded`;
-
-}else if (favoretKind == `Petrol`){
-    message = `You neen to learn more about technology`;
+}else if (favoriteKind == 'Hybrid'){
+    message = 'You are opin minded';
+}else if (favoriteKind == 'Petrol'){
+    message = 'You neen to learn more about technology';
 
     
 }else{
-    message = `welcome`
+    message = 'welcome'
 }
 
-document.write(`<h3>` + message + `</h3>`);
+document.write('<h3>' + message + '</h3>'); */
 
+var order = prompt('What is your favorite type of vehicles electric or hybrid?')
+
+while (order !== 'hybrid' && order !== 'electric') {
+    order = prompt('Please write only electric or hybrid')
+console.log(order)
+}
+console.log(order)
+
+var vehicle = '';
+
+if (order === 'hybrid') {
+    vehicle = '<img src= "pic/hybrid-car.jpg"/> ';
+
+} else if (order === 'electric') {
+
+    vehicle = '<img src="pic/electric-car.jpg"/>' ;
+
+
+}
+
+console.log (result)
+
+var result ="";
+
+
+var numBrands =prompt('How many Brands do you want to see?');
+
+for(var i = 0 ; i<numBrands ; i++){
+    console.log(i);
+    result = result + vehicle;
+    console.log (result)
+}
+
+console.log (result)
+document.write(result);
